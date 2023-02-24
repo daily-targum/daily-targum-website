899eb211dfdbafffbd1f9454e9239b52bd176c01
Abdul Uddin <65580428+auddin431@users.noreply.github.com>
2023-02-23 21:25:41 -0500

    feat: rearranged sports tags and fixed regex (#69)

--- src/components/TagBar.tsx
+++ src/components/TagBar.tsx
@@ -1,57 +1,55 @@
-import * as React from 'react'
-import styles from './TagBar.module.scss';
-import cn from 'classnames';
+import * as React from "react";
+import styles from "./TagBar.module.scss";
+import cn from "classnames";
 
 export function TagBar({
   tags,
   onChange,
   value,
-  style
+  style,
 }: {
-  tags: string[]
-  onChange: (val: string | null) => any
-  value: string | null
-  style?: React.CSSProperties
+  tags: string[];
+  onChange: (val: string | null) => any;
+  value: string | null;
+  style?: React.CSSProperties;
 }) {
   return (
-    <div 
+    <div
       className={styles.tagBar}
       style={style}
-      aria-label='Tag Selector Bar'
-      data-tooltip-position='none'
+      aria-label="Tag Selector Bar"
+      data-tooltip-position="none"
     >
-      <button 
-        className={cn(
-          styles.tag,
-          {
-            [styles.tagSelected]: value === null
-          }
-        )}
+      <button
+        className={cn(styles.tag, {
+          [styles.tagSelected]: value === null,
+        })}
         onClick={() => onChange(null)}
-        aria-label='Clear selected tag'
-        data-tooltip-position='none'
-      > 
+        aria-label="Clear selected tag"
+        data-tooltip-position="none"
+      >
         all
       </button>
 
-      {tags.map(tag => (
-        <button 
-          className={cn(
-            styles.tag,
-            {
-              [styles.tagSelected]: tag === value
-            }
-          )}
-          key={tag}
-          onClick={() => {
-            onChange(tag);
-          }}
-          aria-label={tag.replace(/s-/g, "'s ")+" tag"}
-          data-tooltip-position='none'
-        >
-          {tag.replace(/s-/g, "'s ")}
-        </button>
-      ))}
+      {tags.map((tag) => {
+        const newTag = tag.replace(/-/g, " ");
+        const displayTag = newTag.replace(/ns/g, "n's ");
+        return (
+          <button
+            className={cn(styles.tag, {
+              [styles.tagSelected]: tag === value,
+            })}
+            key={tag}
+            onClick={() => {
+              onChange(tag);
+            }}
+            aria-label={displayTag + " tag"}
+            data-tooltip-position="none"
+          >
+            {displayTag}
+          </button>
+        );
+      })}
     </div>
   );
 }
